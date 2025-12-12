e325bdefe403aae26296a1bd595e7508829f8f4a
Dominic Bazina-Grolinger <dbazinag@uwo.ca>
2025-12-12 14:36:27 +0100

    Fix: removed unecessary file

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: process.env.NODE_ENV === 'production'
-		? '/dataisadangerousgame-frontend/'
-		: '/'
+	publicPath:
+		process.env.NODE_ENV === 'production'
+			? '/dataisadangerousgame-frontend/'
+			: '/',
 });
-
